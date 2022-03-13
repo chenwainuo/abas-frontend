1a04665b90be26c3cb956bea4e1a3a63d47c0d06
Jose <jose96043@gmail.com>
2022-03-12 22:38:51 -0500

    typescript ignore build

--- next.config.js
+++ next.config.js
@@ -4,9 +4,13 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
+  // todo remove this once everything is cleaned up
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  //--------------------------
   eslint: {
     dirs: ['.'],
-    ignoreDuringBuilds: true,
   },
   poweredByHeader: false,
   trailingSlash: true,
