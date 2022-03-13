da266fbad4d486feb5f81a073b577f6ac3836fcd
Jose <jose96043@gmail.com>
2022-03-12 22:50:48 -0500

    ignore

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = withBundleAnalyzer({
   //--------------------------
   eslint: {
     dirs: ['.'],
-    ignoreBuildErrors: true,
+    ignoreDuringBuilds: true,
   },
   poweredByHeader: false,
   trailingSlash: true,
