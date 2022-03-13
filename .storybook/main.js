b35334e834653027ba8387dcbd7e073b3a658eee
Jose <jose96043@gmail.com>
2022-03-12 21:13:50 -0500

    implement modal ui for deposits

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,8 +8,23 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    "@storybook/preset-create-react-app",
+    {
+      /**
+       * Fix Storybook issue with PostCSS@8
+       * @see https://github.com/storybookjs/storybook/issues/12668#issuecomment-773958085
+       */
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
+  "core": {
+    "builder": "webpack5"
+  },
   webpackFinal: async (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
--- src/components/DespositModal.tsx
+++ src/components/DespositModal.tsx
@@ -5,80 +5,90 @@ import React from 'react';
  */
 export const DepositModal = () => {
   return (
-    <div
-      className="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-      id="exampleModal"
-      tabIndex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog relative w-auto pointer-events-none">
-        <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-          <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-            <h5
-              className="text-xl font-medium leading-normal text-gray-800"
-              id="exampleModalLabel"
+    <>
+      <label htmlFor="my-modal-3" className="btn modal-button">
+        Deposit
+      </label>
+
+      <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+      <div className="modal rounded-md">
+        <div className="modal-box relative p-0 rounded-md">
+          {/* <h3 className="text-lg font-bold left-2">
+            Congratulations random Interner user!
+          </h3>
+          <label
+            htmlFor="my-modal-3"
+            className="btn btn-sm btn-circle absolute right-2 top-2"
+          >
+            ✕
+          </label> */}
+          <div className="flex justify-between items-start rounded-t border-b dark:border-gray-600 pt-5 pb-5 pr-7 pl-7">
+            <h3 className="text-xl font-semibold text-white-600 lg:text-2xl dark:text-white">
+              Deposit
+            </h3>
+            <label
+              htmlFor="my-modal-3"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-toggle="defaultModal"
             >
-              Modal title
-            </h5>
-            <button
-              type="button"
-              className="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </label>
           </div>
-          <div className="modal-body relative p-4">
-            Modal body text goes here.
-          </div>
-          <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-            <button
-              type="button"
-              className="px-6
-            py-2.5
-            bg-purple-600
-            text-white
-            font-medium
-            text-xs
-            leading-tight
-            uppercase
-            rounded
-            shadow-md
-            hover:bg-purple-700 hover:shadow-lg
-            focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-            active:bg-purple-800 active:shadow-lg
-            transition
-            duration-150
-            ease-in-out"
-              data-bs-dismiss="modal"
-            >
-              Close
-            </button>
-            <button
-              type="button"
-              className="px-6
-        py-2.5
-        bg-blue-600
-        text-white
-        font-medium
-        text-xs
-        leading-tight
-        uppercase
-        rounded
-        shadow-md
-        hover:bg-blue-700 hover:shadow-lg
-        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-        active:bg-blue-800 active:shadow-lg
-        transition
-        duration-150
-        ease-in-out
-        ml-1"
-            >
-              Save changes
-            </button>
-          </div>
+
+          <p className="py-4 pr-7 pl-7">
+            <div className="form-control w-full ">
+              <label className="label">
+                <span className="label-text text-md">Deposit Amount</span>
+                <button>
+                  <span className="label-text-alt underline">Max</span>
+                </button>
+              </label>
+              <div className="relative">
+                <input
+                  type="text"
+                  placeholder="100"
+                  className="input input-bordered w-full"
+                />
+                <span className="absolute inset-y-0 right-0 flex items-center pr-2">
+                  <div>USDC</div>
+                </span>
+              </div>
+              <label className="label">
+                <span className="label-text-alt">
+                  <div>Remaining Max Deposit: </div>
+                  <div>Wallet USDC Balance: </div>
+                </span>
+              </label>
+            </div>
+          </p>
+          <p className="py-4 pr-7 pl-7 pt-10">
+            <div className="text-sm">
+              Don&apos;t have USDC?{' '}
+              <button>
+                <span className="underline">Buy some here</span>
+              </button>
+            </div>
+            <div className="divider mb-2"></div>
+            <div className="flex justify-between pb-4">
+              <div>Total Colateral</div>
+              <div>02</div>
+            </div>
+
+            <button className="btn btn-wide w-full">Confirm Deposit</button>
+          </p>
         </div>
       </div>
-    </div>
+    </>
   );
 };
